e3eb76f354c50b5afabc53abc5f34697791442ca
Hugo Leonardo Costa e Silva <hugoleodev@gmail.com>
2015-06-28 00:13:55 -0300

    Adicionando o slug do filme como field prepopulado no admin

--- play/core/admin.py
+++ play/core/admin.py
@@ -4,5 +4,6 @@ from play.core.models import Filme
 
 class FilmeAdmin(admin.ModelAdmin):
     list_display = ('nome', 'sinopse', 'image_tag', 'created_at')
+    prepopulated_fields = {'slug': ('nome',)}
 
 admin.site.register(Filme, FilmeAdmin)
